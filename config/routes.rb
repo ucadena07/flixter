7807b1c1653498d15e1ec1f54cebc5a399cbfcca
Ulises Cadena <ucadena07@gmail.com>
2019-10-16 17:36:55 +0000

    allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
 
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end 
+
 
 
 
